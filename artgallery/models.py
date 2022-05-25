3da912f1b966528d4600688174d0eb50fc64587a
Jeff <Xavierjeff451@gmail.com>
2022-05-25 09:51:57 +0300

    initial migration

--- artgallery/models.py
+++ artgallery/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-
+class Images(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField(blank=True)
