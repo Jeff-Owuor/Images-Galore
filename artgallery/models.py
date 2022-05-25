161d91cc0dde28ac3f57b26ca2eeb348998d7cb2
Jeff <Xavierjeff451@gmail.com>
2022-05-25 10:02:33 +0300

    addition of location and category models

--- artgallery/models.py
+++ artgallery/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class Images(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to = 'gallery/')
     image_name = models.CharField(max_length=30)
     image_description = models.TextField(blank=True)
+    
+    
+class ImageLocation(models.Model):
+    location = models.CharField(max_length=60)
+    
+class ImageCategory(models.Model):
+    category = models.CharField(max_length=60)
