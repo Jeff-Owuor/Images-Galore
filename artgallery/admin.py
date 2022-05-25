139096194f5d861baeeb43b4cc93760d57bcb8b3
Jeff <Xavierjeff451@gmail.com>
2022-05-25 10:46:48 +0300

    addition of admin

--- artgallery/admin.py
+++ artgallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Images,ImageLocation,ImageCategory
+
+admin.site.register(Images)
+admin.site.register(ImageLocation)
+admin.site.register(ImageCategory)
 
 # Register your models here.
--- artgallery/models.py
+++ artgallery/models.py
@@ -1,13 +1,32 @@
+from email.mime import image
 from django.db import models
 
-class Images(models.Model):
-    image = models.ImageField(upload_to = 'gallery/')
-    image_name = models.CharField(max_length=30)
-    image_description = models.TextField(blank=True)
-    
-    
+
 class ImageLocation(models.Model):
     location = models.CharField(max_length=60)
     
 class ImageCategory(models.Model):
     category = models.CharField(max_length=60)
+    
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'gallery/')
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField(blank=True)
+    image_location = models.ForeignKey(ImageLocation,on_delete=models.CASCADE,default=0)
+    image_category = models.ForeignKey(ImageCategory,on_delete=models.CASCADE,default=0)
+    
+    # def get_image_by_id(cls):
+    #     images = cls.objects.get(id)
+    #     return images
+    # @classmethod
+    # def filter_by_location(cls,locale):
+    #     image_by_location = cls.objects.filter(image_location = locale)   
+    #     return image_by_location
+    
+    def save_image(self):
+        self.save()
+        
+    class Meta:
+        ordering = ['image_name']
+    
+    
